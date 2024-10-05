f605966af27c9ab8d37c80b614effd207438e6f1
xetob <xetob1122@gmail.com>
2024-10-06 01:15:59 +0300

    Update lineage_fire.mk

--- lineage_fire.mk
+++ lineage_fire.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from generic device
+# Inherit from device makefile.
 $(call inherit-product, device/xiaomi/fire/device.mk)
 
 PRODUCT_NAME := lineage_fire
